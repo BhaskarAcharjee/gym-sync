0f2706d3f63563ad9ef6e074636901c20d8b96cb
BhaskarAcharjee <bhaskaracharjee2002@gmail.com>
2024-03-12 01:26:35 +0530

    Navbar & Hero Banner content added

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Box, Stack, Typography, Button } from "@mui/material";
+
+import HeroBannerImage from "../assets/images/gym-banner.png";
+
+const HeroBanner = () => {
+  return (
+    <Box
+      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      position="relative"
+      p="20px"
+    >
+      <Typography color="#ff2625" fontWeight="600" fontSize="26px" mt="-120px">
+        Fitness Hub
+      </Typography>
+
+      <Typography
+        fontWeight={700}
+        sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+        mb="23px"
+        mt="30px"
+      >
+        Sweat, Shine, and <br/>Keep Pushing
+      </Typography>
+      <Typography fontSize="22px" lineHeight="30px" mb={4}>
+      Achieve Your Fitness Goals, One Workout at a Time
+      </Typography>
+      <Button
+        variant="contained"
+        color="error"
+        href="#exercises"
+        sx={{ backgroundColor: "#ff2625", padding: "10px" }}
+      >
+        Explore Exercises
+      </Button>
+      <Typography
+        fontWeight={600}
+        color="#ff2625"
+        sx={{ opacity: 0.1, display: { lg: "block", xs: "none" } }}
+        fontSize={200}
+      >
+        Exercise
+      </Typography>
+      <img src={HeroBannerImage} alt="banner" className="hero-banner-img" />
+    </Box>
+  );
+};
+
+export default HeroBanner;
